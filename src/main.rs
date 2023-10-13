68f4c0326bcc030c17379b2acfbebfc27f429efd
Snuwbule <snuwbule@ashtereda.gay>
2023-10-14 02:16:08 +0700

    args checking

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+use std::process;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let usr_args: Vec<String> = env::args().collect();
+    if usr_args.len() < 2 {
+        println!("Heeyyyy gib me one arg, u silly!\n(dat arg being da file u wanna run)");
+        process::exit(1);
+    } else if usr_args.len() >2 {
+        println!("That's too much args! you silly!");
+        process::exit(1);
+    }
+    
+    //let mut bites;
+    //let mut cur_bite : i32;  
 }
